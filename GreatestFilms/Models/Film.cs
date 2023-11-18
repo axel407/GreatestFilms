ac914909618898f2d9725e05635f7e8bab71e53a
Axel7 <Axel_Jkit@protonmail.com>
2023-11-18 16:45:43 +0200

    Добавление классов Film и FilmContext Постеров фильмов

---
+++ GreatestFilms/Models/Film.cs
@@ -0,0 +1,21 @@
+﻿namespace GreatestFilms.Models
+{
+    public class Film
+    {
+        //Film Id
+        public int Id { get; set; }
+        //Film Name
+        public string Name { get; set; }
+        //Filml`s director name
+        public string DirectorName { get; set; }
+        //Film genre
+        public string Genre { get; set; }
+        //Release date
+        public string Date { get; set; }
+        //Film discription
+        public string Description { get; set; }
+        //Path to poster image
+        public string Poster { get; set; }
+
+    }
+}
---
+++ GreatestFilms/Models/FilmContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GreatestFilms.Models
+{
+    public class FilmContext:DbContext
+    {
+        public DbSet<Film> Films { get; set; }
+        public FilmContext(DbContextOptions<FilmContext> options) : base(options)
+        {
+            if(Database.EnsureCreated())
+            {
+                Films.Add(new Film { Name="Atlantis: The Lost Empire", DirectorName= "Gary Trousdale", Genre="Action-Adventure", Poster="/Images/Atlantis.jpg", Date="2001", Description=""});
+                Films.Add(new Film { Name="Blade Runner: 2049", DirectorName= "Denis Villeneuve", Genre="Science Fiction", Poster="/Images/BladeRunner2049.jpg", Date="2017", Description="" });
+            }
+        }
+
+    }
+}
