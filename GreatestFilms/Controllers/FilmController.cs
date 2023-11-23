d5eba9b527617518b66ff7e7bf43bc1476cb5820
Axel7 <Axel_Jkit@protonmail.com>
2023-11-23 21:18:14 +0200

    minor changes

---
+++ GreatestFilms/Controllers/FilmController.cs
@@ -0,0 +1,26 @@
+﻿using GreatestFilms.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+namespace GreatestFilms.Controllers
+{
+    public class FilmController : Controller
+    {
+        private readonly FilmContext _context;
+
+        public FilmController(FilmContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return _context.Films != null ?
+                View(await _context.Films.ToListAsync()) : 
+                Problem("Entity set 'FilmContext.Films' is null.");
+        }
+
+        
+    }
+}
