2af0e3b322f81ed0592b6e05197dc5a6436e1367
Axel7 <Axel_Jkit@protonmail.com>
2023-11-24 21:03:03 +0200

    Added GET functions in FilmsController -Details -Create -Edit -Delete

--- GreatestFilms/Controllers/FilmsController.cs
+++ GreatestFilms/Controllers/FilmsController.cs
@@ -20,6 +20,63 @@ namespace GreatestFilms.Controllers
                 View(await _context.Films.ToListAsync()) : 
                 Problem("Entity set 'FilmContext.Films' is null.");
         }
-        
+
+        //GET: Details
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var film = await _context.Films.SingleOrDefaultAsync(m => m.Id == id);
+
+            if (film == null)
+            {
+                return NotFound();
+            }
+
+            return View(film);
+        }
+
+        //GET: Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //GET: Edit
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Films == null)
+            {
+                return NotFound();
+            }
+
+            var film = await _context.Films.FindAsync(id);
+            if (film == null)
+            {
+                return NotFound();
+            }
+            return View(film);
+        }
+
+        //GET: Delete
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if(id == null || _context.Films == null)
+            {
+                return NotFound();
+            }
+
+            var film = await _context.Films.FirstOrDefaultAsync(m => m.Id == id);
+            if (film == null)
+            {
+                return NotFound();
+            }
+
+            return View(film);
+        }
+
     }
 }
