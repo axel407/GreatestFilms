2df9d10fefd4bf1b6e72494d65257678a2fa814a
Axel7 <Axel_Jkit@protonmail.com>
2023-12-01 21:56:22 +0200

    validation added

--- GreatestFilms/Models/Film.cs
+++ GreatestFilms/Models/Film.cs
@@ -10,19 +10,27 @@ namespace GreatestFilms.Models
     public class Film
     {
         //Film Id
+        [Required(ErrorMessage = "Поле должно быть установлено")]
         public int Id { get; set; }
         //Film Name
+        [Required(ErrorMessage = "Поле должно быть установлено")]
         public string Name { get; set; }
         //Filml`s director name
+        [Required(ErrorMessage = "Поле должно быть установлено")]
         public string DirectorName { get; set; }
         //Film genre
+        [Required(ErrorMessage = "Поле должно быть установлено")]
         public string Genre { get; set; }
         //Release date
+        [Required(ErrorMessage = "Поле должно быть установлено")]
+        [RegularExpression(@"[0-9]", ErrorMessage = "Некорректный адрес")]
         public string Date { get; set; }
         //Film discription
         [MaxLength(500)]
+        [Required(ErrorMessage = "Поле должно быть установлено")]
         public string Description { get; set; }
         //Path to poster image
+        [Required(ErrorMessage = "Поле должно быть установлено")]
         public string Poster { get; set; }
 
     }
